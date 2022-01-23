f46cab95225a46c7f2d3af65797d758ace371352
Dennis Shen <dshen@umass.edu>
2022-01-23 17:36:31 -0500

    First Commit

---
+++ BlahHelperBot/BlahHelperCommands.cs
@@ -0,0 +1,85 @@
+﻿using Discord.Commands;
+
+namespace BlahHelperBot
+{
+    // Keep in mind your module **must** be public and inherit ModuleBase.
+    // If it isn't, it will not be discovered by AddModulesAsync!
+    [Group("blah")]
+    public class BlahHelperCommands : ModuleBase<SocketCommandContext>
+    {
+        // ~say hello world -> hello world
+        [Command("say")]
+        [Summary("Echoes a message.")]
+        public Task SayAsync([Remainder][Summary("The text to echo")] string echo)
+        {
+            return ReplyAsync(echo);
+        }
+
+        [Command("sayNumber")]
+        [Summary("echoes a message with a number")]
+        public Task SayAsync([Summary("number to echo")] int number, [Remainder][Summary("The text to echo")] string echo)
+        {
+            return ReplyAsync(number.ToString() + " hi " + echo);
+        }
+
+        //takes a bit sale ad and a cost per bit 
+        //returns the message needed to enter the necessary bits into a karuta trade as well as the cost of the bits
+        [Command("buy")]
+        [Summary("buys bits")]
+        public async Task BuyBits([Summary("rate to buy bits")] int bitRate, [Remainder][Summary("the sale ad")] string bitAd)
+        {
+            string[] bitTypes = new string[] {
+                "essence", "iron", "salt", "uranium", "leaf", "magma", "clay", "wax", "wool", "flower",
+                "oil", "bone", "copper", "ice", "quartz", "stone", "sugar", "wood", "zinc"
+            };
+
+            int totalCost = 0;
+            string finalSale = "";
+
+            //remove all spaces and punctuation from the bit ad and change to lowercase
+            bitAd = new string(bitAd.Where(c => !(char.IsPunctuation(c)||char.IsWhiteSpace(c))).ToArray()).ToLower();
+
+
+            //Every time we find numbers in the bit ad, we will then look for a valid bit type after that number.
+            //If we find a valid bit type, then we will assume that the number indicates the amount of that bit.
+            //We will buy as many bits as possible of that type of bit at a specified rate
+            string curDigits = "";
+            for (int i = 0; i <bitAd.Length; i++)
+            {
+                if (char.IsDigit(bitAd[i]))
+                {
+                    curDigits += bitAd[i];
+                }
+                else if (!curDigits.Equals(""))
+                {
+                    foreach (string bitType in bitTypes)
+                    {
+                        if (bitAd.IndexOf(bitType, i) == i)
+                        {
+                            int bitTotal = int.Parse(curDigits);
+                            int ticketCost = bitTotal / bitRate;
+                            totalCost += ticketCost;
+                            if (ticketCost > 0)
+                            {
+                                finalSale += (bitRate * ticketCost).ToString() + " " + bitType + " bit, ";
+                            }
+                            break;
+                        }
+                    }
+                    curDigits = "";
+                }
+            }
+
+            if(finalSale.Length == 0)
+            {
+                finalSale = "fail";
+            }
+            else
+            {
+                finalSale = finalSale + "\n\nfor " + totalCost.ToString() + " tickets";
+            }
+
+            await ReplyAsync(finalSale);
+        }
+    }
+}
---
+++ BlahHelperBot/Program.cs
@@ -0,0 +1,70 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace BlahHelperBot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private readonly DiscordSocketClient _client;
+        private readonly CommandService _commandService;
+
+        private Program()
+        {
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                // How much logging do you want to see?
+                LogLevel = LogSeverity.Info,
+
+                // If you or another service needs to do anything with messages
+                // (eg. checking Reactions, checking the content of edited/deleted messages),
+                // you must set the MessageCacheSize. You may adjust the number as needed.
+                MessageCacheSize = 50,
+
+                // If your platform doesn't have native WebSockets,
+                // add Discord.Net.Providers.WS4Net from NuGet,
+                // add the `using` at the top, and uncomment this line:
+                //WebSocketProvider = WS4NetProvider.Instance
+            });
+
+            _commandService = new CommandService(new CommandServiceConfig
+            {
+                LogLevel = LogSeverity.Info,
+                CaseSensitiveCommands = false,
+            });
+
+            _client.Log += Log;
+            _commandService.Log += Log;
+        }
+
+        public async Task MainAsync()
+        {
+
+            CommandHandler commandHandler = new CommandHandler(_client, _commandService);
+            await commandHandler.InstallCommandsAsync();
+
+            //  You can assign your bot token to a string, and pass that in to connect.
+            //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+            var token = "OTEyNTI0NDM3NTg3MzYxODMy.YZxMmw.wTEYZgUpZtSJrlNNj0-Vs55aujk";
+
+            // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
+            // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
+            // var token = File.ReadAllText("token.txt");
+            // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(Timeout.Infinite);
+        }
+        private Task Log(LogMessage msg)
+        {
+            //Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
